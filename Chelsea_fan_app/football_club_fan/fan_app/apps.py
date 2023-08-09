de5982ffbca862c69cf53beb035b56b848bf1b62
aleksandaryovchev <alexsander.i.yovchev@gmail.com>
2023-08-09 20:32:16 +0300

    .

--- Chelsea_fan_app/football_club_fan/fan_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FanAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'football_club_fan.fan_app'
--- README.md
+++
@@ -1,2 +0,0 @@
-# Softuni_Python_Web_Framework_Regular_Exam
- Курсов проект за редовен изпит след курса Python Web Framework - юни 2023
